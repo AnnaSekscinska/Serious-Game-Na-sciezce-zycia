dd3ba6212f704d5af5e4345a879b112ce8b70a4c
Anna Sekscinska <annasekscinska03@gmail.com>
2025-10-28 20:47:35 +0100

    Add Movement to pixel

--- Game1.cs
+++ Game1.cs
@@ -8,6 +8,9 @@ public class Game1 : Game
 {
     private GraphicsDeviceManager _graphics;
     private SpriteBatch _spriteBatch;
+    Texture2D pixel;
+    private Rectangle pixelRect;
+    private Vector2 pixelPosition;
 
     public Game1()
     {
@@ -26,16 +29,30 @@ public class Game1 : Game
     protected override void LoadContent()
     {
         _spriteBatch = new SpriteBatch(GraphicsDevice);
-
+        pixel = new Texture2D(GraphicsDevice, 1, 1);
+        pixel.SetData(new Color[] {Color.White});
+        pixelPosition = new Vector2(0,0);
+        pixelRect = new Rectangle(0 , 0, 10, 15);
         // TODO: use this.Content to load your game content here
     }
 
     protected override void Update(GameTime gameTime)
     {
-        if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+        if (Keyboard.GetState().IsKeyDown(Keys.Escape))
             Exit();
 
+        var keyboardState = Keyboard.GetState();
+        if (keyboardState.IsKeyDown(Keys.Down)) {
+            pixelPosition += new Vector2(0,2);
+        } else if (keyboardState.IsKeyDown(Keys.Up)){
+             pixelPosition += new Vector2(0,-2);
+        } else if (keyboardState.IsKeyDown(Keys.Left)){
+             pixelPosition += new Vector2(-2,0);
+        } else if (keyboardState.IsKeyDown(Keys.Right)){
+             pixelPosition += new Vector2(2,0);
+        }
         // TODO: Add your update logic here
+            pixelRect = new Rectangle((int)pixelPosition.X, (int)pixelPosition.Y, 10, 15); 
 
         base.Update(gameTime);
     }
@@ -43,9 +60,13 @@ public class Game1 : Game
     protected override void Draw(GameTime gameTime)
     {
         GraphicsDevice.Clear(Color.CornflowerBlue);
+        _spriteBatch.Begin();
 
-        // TODO: Add your drawing code here
 
+        _spriteBatch.Draw(pixel, pixelRect, Color.Red);
+        
+        
+        _spriteBatch.End();
         base.Draw(gameTime);
     }
 }
